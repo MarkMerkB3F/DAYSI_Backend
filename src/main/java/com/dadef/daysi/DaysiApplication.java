fd3fdf6caa2a4ef3fde051b525262848bfcee78a
The Sleeper <IanSchenk@gmx.de>
2025-01-13 10:22:19 +0100

    Initial Commit

---
+++ src/main/java/com/dadef/daysi/DaysiApplication.java
@@ -0,0 +1,14 @@
+package com.dadef.daysi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysiApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/dadef/daysi/DaysiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadef.daysi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaysiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
