bdef01120a6794d4950eab82b3e79c5287ba4372
Nils Münzer <nils.muenzer@gmx.de>
2025-01-16 13:26:55 +0100

    mapper for all entities

--- src/main/java/com/dadef/daysi/mapper/BigChug.java
+++ src/main/java/com/dadef/daysi/mapper/BigChug.java
@@ -1,6 +1,10 @@
 package com.dadef.daysi.mapper;
 
+import com.dadef.daysi.dtos.AppointmentDTO;
+import com.dadef.daysi.dtos.NoteDTO;
 import com.dadef.daysi.dtos.TodoDTO;
+import com.dadef.daysi.entities.Appointment;
+import com.dadef.daysi.entities.Note;
 import com.dadef.daysi.entities.Todo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,6 +12,15 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface BigChug {
 
-    @Mapping(target = "category", source = "todo.category")
     TodoDTO mapTodoEntityToTodoDto(Todo todo);
+
+    Todo mapTodoDtoToTodoEntity(TodoDTO todoDto);
+
+    NoteDTO mapNoteEntityToNoteDto(Note note);
+
+    Note mapNoteDtoToNoteEntity(NoteDTO noteDto);
+
+    AppointmentDTO mapAppointmentEntityToAppointmentDto(Appointment appointment);
+
+    Appointment mapAppointmentDtoToAppointmentEntity(AppointmentDTO appointmentDto);
 }
