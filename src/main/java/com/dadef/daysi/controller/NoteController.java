45696da408a3391b0c9fbfda6633b49e958db802
Nils Münzer <nils.muenzer@gmx.de>
2025-01-16 14:01:30 +0100

    return dto when getAllNotes

--- src/main/java/com/dadef/daysi/controller/NoteController.java
+++ src/main/java/com/dadef/daysi/controller/NoteController.java
@@ -1,23 +1,37 @@
 package com.dadef.daysi.controller;
 
+import com.dadef.daysi.dtos.AppointmentDTO;
+import com.dadef.daysi.dtos.NoteDTO;
+import com.dadef.daysi.entities.Appointment;
 import com.dadef.daysi.entities.Note;
+import com.dadef.daysi.mapper.BigChug;
 import com.dadef.daysi.repositorys.NoteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class NoteController {
+
     private final NoteRepository repository;
 
-    NoteController(NoteRepository repository) {
+    private final BigChug bigChug;
+
+    NoteController(NoteRepository repository, BigChug bigChug) {
         this.repository = repository;
+        this.bigChug = bigChug;
     }
 
     @GetMapping("/notes")
-    public List<Note> getAllNotes(){
+    public List<NoteDTO> getAllNotes(){
         System.out.println("Alle vorhandenen Notizen wurden versendet");
-        return repository.getAllNotes();
+        List<Note> notes = repository.getAllNotes();
+        List<NoteDTO> noteDtos = new ArrayList<NoteDTO>(notes.size());
+        for (Note note : notes) {
+            noteDtos.add(bigChug.mapNoteEntityToNoteDto(note));
+        }
+        return noteDtos;
     }
 }
